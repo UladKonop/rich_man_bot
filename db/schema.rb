345130535c85cf189838978e7fb617412a47d321
Uladzislau Kanapliou <vladknpljov@gmail.com>
2025-04-16 18:32:20 +0200

    Remove tenders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2020_09_03_122438) do
+ActiveRecord::Schema[7.1].define(version: 2020_09_01_081624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,15 +59,6 @@ ActiveRecord::Schema[7.1].define(version: 2020_09_03_122438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
-  end
-
-  create_table "tenders", force: :cascade do |t|
-    t.string "url"
-    t.string "header"
-    t.text "body"
-    t.jsonb "fields", default: {}
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
