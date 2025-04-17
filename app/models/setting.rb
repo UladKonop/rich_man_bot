423a6eabfa1c5c5ebf239cf11b72a7e4f03add69
Uladzislau Kanapliou <vladknpljov@gmail.com>
2025-04-17 20:39:54 +0200

    Remove unused code

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -6,15 +6,6 @@ class Setting < ApplicationRecord
 
   scope :active, -> { joins(user: :subscription).merge(Subscription.active).where(active: true) }
 
-  def filtered_tenders
-    Tender.by_industries(industries).by_keywords(keywords)
-  end
-
-  def industries
-    f = filters || {}
-    f['industries'] ||= []
-  end
-
   # only users with active subscription will be notifyed
   def active?
     active && subscription.active?
@@ -28,33 +19,5 @@ class Setting < ApplicationRecord
 
   def deactivate!
     update(active: false)
-  end
-
-  def add_industries!(industries)
-    add_filter!('industries', industries)
-  end
-
-  def reset_industries!
-    add_filter!('industries', [])
-  end
-
-  def add_keywords!(raw_keywords)
-    new_keywords = raw_keywords.join(', ').split(/[,\.\s]+/)
-    update(keywords: new_keywords)
-  end
-
-  def reset_keywords!
-    update(keywords: [])
-  end
-
-  def add_filter!(key, value)
-    f = filters || {}
-    f[key] = value
-    self.filters = f
-    save!
-  end
-
-  def pretty_keywords
-    keywords.join(", ")
   end
 end
--- db/migrate/20200813214939_create_settings.rb
+++ db/migrate/20200813214939_create_settings.rb
@@ -2,9 +2,6 @@ class CreateSettings < ActiveRecord::Migration[7.1]
   def change
     create_table :settings do |t|
       t.references :user
-      t.jsonb :filters
-      t.integer :last_sended_tender_id
-      t.string :keywords
 
       t.timestamps
     end
